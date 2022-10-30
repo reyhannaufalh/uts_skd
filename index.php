90e60bc60934fef357ca41aac50dafc347082d6e
reyhannaufalh <91453451+reyhannaufalh@users.noreply.github.com>
2022-10-30 16:50:54 +0700

    update index

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 (@include('app/koneksi.php')) or die('Tidak bisa terkoneksi ke database');
+require 'app/Conversion.php';
 session_start();
+
+if (isset($_GET['hash'])) {
+    $tipe = $_GET['hash'];
+} else if (isset($_POST['hash'])) {
+    $tipe = $_POST['hash'];
+} else {
+    $tipe = 'CC';
+}
 
 ?>
 <!DOCTYPE html>
@@ -32,52 +41,319 @@ session_start();
             <?php } ?>
         </nav>
         <main class="landing-page">
-            <div class="main main-1">
-                <div class="main-header">
-                    <p id="main-title">Text</p>
-                    <h2>Plain Text</h2>
-                </div>
-                <div class="main-input">
-                    <textarea name="" id="plain" cols="30"
-                        rows="10">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</textarea>
-                </div>
-            </div>
             <div class="main main-2">
+                <form action="" method="get">
+                    <select name="hash" id="">
+                        <option value="CC" <?= ($tipe == 'CC') ? 'selected' : ''; ?>>Caesar Chiper</option>
+                        <option value="VC" <?= ($tipe == 'VC') ? 'selected' : ''; ?>>VigenereChiper</option>
+                        <option value="AES-128-CBC" <?= ($tipe == 'AES-128-CBC') ? 'selected' : ''; ?>>AES-128-CBC
+                        </option>
+                        <option value="AES-256-CBC" <?= ($tipe == 'AES-256-CBC') ? 'selected' : ''; ?>>AES-256-CBC
+                        </option>
+                        <option value="AES-128-CTR" <?= ($tipe == 'AES-128-CTR') ? 'selected' : ''; ?>>AES-128-CTR
+                        </option>
+                        <option value="AES-256-CTR" <?= ($tipe == 'AES-256-CTR') ? 'selected' : ''; ?>>AES-256-CTR
+                        </option>
+                        <option value="RSA" <?= ($tipe == 'RSA') ? 'selected' : ''; ?>>RSA</option>
+                        <option value="RC4" <?= ($tipe == 'RC4') ? 'selected' : ''; ?>>RC4</option>
+                    </select>
+
+                    <button type="submit">Pilih</button>
+                </form>
+
+                <div class="key">
+                    <form action="" method="post">
+                        <input type="hidden" name="hash" value="<?= $tipe ?>">
+                        <table>
+                            <tbody>
+                                <?php
+                                switch ($tipe) {
+                                    case 'CC':
+                                ?>
+                                <tr>
+                                    <td>Key</td>
+                                    <td>
+                                        <input type="number" name="key">
+                                    </td>
+                                </tr>
+                                <?php
+                                        break;
+                                    case 'VC':
+                                    ?>
+                                <tr>
+                                    <td>Key</td>
+                                    <td>
+                                        <input type="text" name="key">
+                                    </td>
+                                </tr>
+                                <?php
+                                        break;
+                                    case 'AES-128-CBC':
+                                        $aes = new AES();
+                                        $aes->Chiper('aes-128-cbc');
+                                    ?>
+                                <tr>
+                                    <td>Key</td>
+                                    <td>
+                                        <textarea name="key" rows="4" cols="50"><?= $aes->getKey(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Iv</td>
+                                    <td>
+                                        <textarea name="iv" rows="4" cols="50"><?= $aes->getIv(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <?php
+                                        break;
+                                    case 'AES-256-CBC':
+                                        $aes = new AES();
+                                        $aes->Chiper('aes-256-cbc');
+                                    ?>
+                                <tr>
+                                    <td>Key</td>
+                                    <td>
+                                        <textarea name="key" rows="4" cols="50"><?= $aes->getKey(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Iv</td>
+                                    <td>
+                                        <textarea name="iv" rows="4" cols="50"><?= $aes->getIv(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <?php
+                                        break;
+                                    case 'AES-128-CTR':
+                                        $aes = new AES();
+                                        $aes->Chiper('aes-128-ctr');
+                                    ?>
+                                <tr>
+                                    <td>Key</td>
+                                    <td>
+                                        <textarea name="key" rows="4" cols="50"><?= $aes->getKey(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Iv</td>
+                                    <td>
+                                        <textarea name="iv" rows="4" cols="50"><?= $aes->getIv(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <?php
+                                        break;
+                                    case 'AES-256-CTR':
+                                        $aes = new AES();
+                                        $aes->Chiper('aes-256-ctr');
+                                    ?>
+                                <tr>
+                                    <td>Key</td>
+                                    <td>
+                                        <textarea name="key" rows="4" cols="50"><?= $aes->getKey(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Iv</td>
+                                    <td>
+                                        <textarea name="iv" rows="4" cols="50"><?= $aes->getIv(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <?php
+                                        break;
+                                    case 'RSA':
+                                        $rsa = new RSA();
+                                        $rsa->generateKey();
+                                    ?>
+                                <tr>
+                                    <td>Public key</td>
+                                    <td>
+                                        <textarea name="public_key" rows="4"
+                                            cols="50"><?= $rsa->getPublicKey(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Private key</td>
+                                    <td>
+                                        <textarea name="private_key" rows="4"
+                                            cols="50"><?= $rsa->getPrivateKey(); ?></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Key yg digunakan</td>
+                                    <td>
+                                        <select name="selected_key">
+                                            <option value="public">Public key</option>
+                                            <option value="private">Private key</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                                <?php
+                                        break;
+                                    case 'RC4':
+                                    ?>
+                                <tr>
+                                    <td>Key</td>
+                                    <td>
+                                        <input type="text" name="key">
+                                    </td>
+                                </tr>
+                                <?php
+                                        break;
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                </div>
                 <div class="switch-mode">
                     <div class="mode active">
                         <label for="en">Enkripsi</label>
-                        <input type="radio" id="en" name="mode" checked>
+                        <input type="radio" id="en" name="tipe" value="enkripsi" checked>
                     </div>
                     <div class="mode">
                         <label for="de">Dekripsi</label>
-                        <input type="radio" id="de" name="mode">
+                        <input type="radio" id="de" name="tipe" value="dekripsi">
                     </div>
                 </div>
+                <button class="btn-main" name="submit">Konversi</button>
 
-                <select name="" id="">
-                    <option value="caesar">Caesar Cipher</option>
-                    <option value="vignere">Vignere Cipher</option>
-                    <option value="aes">AES</option>
-                    <option value="rsa">RSA</option>
-                    <option value="rc4">RC4</option>
-                </select>
-
-                <div class="key">
-                    <p>Key</p>
-                    <input type="text" name="key">
-                </div>
-
-                <button class="btn-main" name="submit">Submit</button>
             </div>
-            <div class="main main-3">
+            <div class="main main-1">
                 <div class="main-header">
-                    <p>Text</p>
-                    <h2>Cipher Text</h2>
+                    <h2>Input Text</h2>
                 </div>
                 <div class="main-input">
-                    <textarea name="" id="cipher" cols="30"
+                    <textarea name="input" id="plain" cols="30"
                         rows="10">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</textarea>
                 </div>
+                </form>
+            </div>
+
+            <div class="main main-3">
+                <div class="main-header">
+                    <h2>Output Text</h2>
+                </div>
+                <p>
+                    <?php
+                    if (isset($_POST['hash'])) {
+                        switch ($_POST['hash']) {
+                            case 'CC':
+                                $cc = new CaesarChiper();
+                                $cc->setInput($_POST['input']);
+                                $cc->setKey($_POST['key']);
+                                switch ($_POST['tipe']) {
+                                    case 'enkripsi':
+                                        $output = $cc->Enkrip();
+                                        break;
+                                    case 'dekripsi':
+                                        $output = $cc->Dekrip();
+                                        break;
+                                }
+                                break;
+                            case 'VC':
+                                $vc = new VigenereChiper();
+                                $vc->setInput($_POST['input']);
+                                $vc->setKey($_POST['key']);
+                                switch ($_POST['tipe']) {
+                                    case 'enkripsi':
+                                        $output = $vc->Enkrip();
+                                        break;
+                                    case 'dekripsi':
+                                        $output = $vc->Dekrip();
+                                        break;
+                                }
+                                break;
+                            case 'AES-128-CBC':
+                                $aes1 = new AES();
+                                $aes1->setInput($_POST['input']);
+                                $aes1->Chiper($_POST['hash']);
+                                $aes1->setKey($_POST['key']);
+                                $aes1->setIv($aes->getIv());
+                                switch ($_POST['tipe']) {
+                                    case 'enkripsi':
+                                        $output = $aes1->Enkrip();
+                                        break;
+                                    case 'dekripsi':
+                                        $output = $aes1->Dekrip();
+                                        break;
+                                }
+                                break;
+                            case 'AES-256-CBC':
+                                $aes1 = new AES();
+                                $aes1->setInput($_POST['input']);
+                                $aes1->Chiper($_POST['hash']);
+                                $aes1->setKey($_POST['key']);
+                                $aes1->setIv($aes->getIv());
+                                switch ($_POST['tipe']) {
+                                    case 'enkripsi':
+                                        $output = $aes1->Enkrip();
+                                        break;
+                                    case 'dekripsi':
+                                        $output = $aes1->Dekrip();
+                                        break;
+                                }
+                                break;
+                            case 'AES-128-CTR':
+                                $aes1 = new AES();
+                                $aes1->setInput($_POST['input']);
+                                $aes1->Chiper($_POST['hash']);
+                                $aes1->setKey($_POST['key']);
+                                $aes1->setIv($aes->getIv());
+                                switch ($_POST['tipe']) {
+                                    case 'enkripsi':
+                                        $output = $aes1->Enkrip();
+                                        break;
+                                    case 'dekripsi':
+                                        $output = $aes1->Dekrip();
+                                        break;
+                                }
+                                break;
+                            case 'AES-256-CTR':
+                                $aes1 = new AES();
+                                $aes1->setInput($_POST['input']);
+                                $aes1->Chiper($_POST['hash']);
+                                $aes1->setKey($_POST['key']);
+                                $aes1->setIv($aes->getIv());
+                                switch ($_POST['tipe']) {
+                                    case 'enkripsi':
+                                        $output = $aes1->Enkrip();
+                                        break;
+                                    case 'dekripsi':
+                                        $output = $aes1->Dekrip();
+                                        break;
+                                }
+                                break;
+                            case 'RSA':
+                                $rsa = new RSA();
+                                $rsa->setInput($_POST['input']);
+                                $rsa->setPublicKey($_POST['public_key']);
+                                $rsa->setPrivateKey($_POST['private_key']);
+                                switch ($_POST['tipe']) {
+                                    case 'enkripsi':
+                                        $output = $aes1->Enkrip($_POST['selected_key']);
+                                        break;
+                                    case 'dekripsi':
+                                        $output = $aes1->Dekrip($_POST['selected_key']);
+                                        break;
+                                }
+                                break;
+                            case 'RC4':
+                                $cc = new RC4();
+                                $cc->setInput($_POST['input']);
+                                $cc->setKey($_POST['key']);
+                                switch ($_POST['tipe']) {
+                                    case 'enkripsi':
+                                        $output = $cc->Enkrip();
+                                        break;
+                                    case 'dekripsi':
+                                        $output = $cc->Dekrip();
+                                        break;
+                                }
+                                break;
+                        }
+                        echo $output;
+                    }
+                    ?>
+                </p>
             </div>
         </main>
     </div>
